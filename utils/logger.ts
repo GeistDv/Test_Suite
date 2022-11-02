4989cc30a96420eb14998733eff66b00761a1875
jessicaqgomez <49324893+jessicaqgomez@users.noreply.github.com>
2022-11-02 16:02:17 -0500

    change in log4js config and error log file

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,13 +1,10 @@
 import log4js from 'log4js'
-
 log4js.configure({
-    appenders: { append: { type: "file", filename: `logs/error.log` } },
-    categories: { default: { appenders: ["append"], level: "ERROR" } },
-  });
-
-log4js.configure({
-    appenders: { append: { type: "file", filename: `logs/info.log` } },
-    categories: { default: { appenders: ["append"], level: "INFO" } },
+    appenders: { append: { type: "file", filename: `logs/info.log` },
+                 log: { type: "file", filename: `logs/error.log` } },
+    categories: { default: { appenders: ["append"], level: "INFO" },
+                  error:{ appenders: ["log"], level: "ERROR" } },
   });
     
 export const logger = log4js.getLogger();
+export const errorLogger = log4js.getLogger("error");
