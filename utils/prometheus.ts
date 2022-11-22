fb0175b7074f9112466522e3cb72a269e2b40727
automatafh <felipe.henao@testiando.co>
2022-11-21 20:42:39 -0500

    update prometheus integration

--- utils/prometheus.ts
+++ utils/prometheus.ts
@@ -19,7 +19,7 @@ class Prometheus {
             .then(function (response) {
                 results.push(response.data);
                 cont = cont+1;
-                if(cont >= 2){
+                if(cont == 5){
                     console.log("llego a 2")
                     testResults(results);
                     clearInterval(proccess);
@@ -33,45 +33,38 @@ class Prometheus {
                 console.log(error);
             });
         } /*here*/);
-     }, 2 * 1000);
+     }, 5 * 1000);
     }
 }
 
 function testResults(json: any){
     let result = JSON.parse(JSON.stringify(json));
-    for(let res of result){ 
-        console.log(res.data.result);
-    }
-
-}
-
-function proccesJson(json: any){
-
-    interface MyObj {
-        status: string;
-        data: object;
-    }
-    //TODO: Implement logic for get and register metrics
-
-    let jsonResult = JSON.parse(JSON.stringify(json.data.result))
     let cpuValidators: any[] = [];
     let cpuApi: any[] = [];
     let cpuRoot: any[] = [];
-    for(let result of jsonResult){
-        console.log(result.metric.pod);
-        let resultValue = result.value[1];
-        if(result.metric.pod.includes("api")){
-            cpuApi.push(resultValue);
-        }
-        else if(result.metric.pod.includes("validator")){
-            console.log(resultValue);
-            cpuValidators.push(resultValue);
-        }
-        else{
-            cpuRoot.push(resultValue)
-        }       
-        // console.log("timestamp: " + result.value[0])
-        // console.log(new Date(result.value[0] * 1000).toUTCString())
+    let i = 0;
+    let j = 0;
+    for(let res of result){
+        console.log(res.data.result[i].value[1]);
+        //let jsonResult = JSON.parse(JSON.stringify(res.data.result[j]));
+        for(let resu of res.data.result){
+            console.log(resu.metric.pod);
+            let resultValue = resu.value[1];
+            if(resu.metric.pod.includes("api")){
+                cpuApi.push(resultValue);
+            }
+            else if(resu.metric.pod.includes("validator")){
+                console.log(resultValue);
+                cpuValidators.push(resultValue);
+            }
+            else{
+                cpuRoot.push(resultValue)
+            }
+            j++;       
+            // console.log("timestamp: " + result.value[0])
+            // console.log(new Date(result.value[0] * 1000).toUTCString())
+            }
+        i++;
     }
     let cpuTotalValidators = sumMetricValues(cpuValidators)
     let cpuTotalApi = sumMetricValues(cpuApi)
@@ -80,8 +73,45 @@ function proccesJson(json: any){
     console.log(cpuTotalApi);
     console.log(cpuTotalRoot);
     console.log(getMaxMetricValue(cpuValidators))
-
 }
+
+// function proccesJson(json: any){
+
+//     interface MyObj {
+//         status: string;
+//         data: object;
+//     }
+//     //TODO: Implement logic for get and register metrics
+
+//     let jsonResult = JSON.parse(JSON.stringify(json.data.result))
+//     let cpuValidators: any[] = [];
+//     let cpuApi: any[] = [];
+//     let cpuRoot: any[] = [];
+//     for(let result of jsonResult){
+//         console.log(result.metric.pod);
+//         let resultValue = result.value[1];
+//         if(result.metric.pod.includes("api")){
+//             cpuApi.push(resultValue);
+//         }
+//         else if(result.metric.pod.includes("validator")){
+//             console.log(resultValue);
+//             cpuValidators.push(resultValue);
+//         }
+//         else{
+//             cpuRoot.push(resultValue)
+//         }       
+//         // console.log("timestamp: " + result.value[0])
+//         // console.log(new Date(result.value[0] * 1000).toUTCString())
+//     }
+//     let cpuTotalValidators = sumMetricValues(cpuValidators)
+//     let cpuTotalApi = sumMetricValues(cpuApi)
+//     let cpuTotalRoot = sumMetricValues(cpuRoot)
+//     console.log(cpuTotalValidators);
+//     console.log(cpuTotalApi);
+//     console.log(cpuTotalRoot);
+//     console.log(getMaxMetricValue(cpuValidators))
+
+// }
 
 function sumMetricValues(values: string[]){
     let total = 0; 
@@ -92,7 +122,6 @@ function sumMetricValues(values: string[]){
 }
 
 function getMaxMetricValue(values: number[]){
-    //Math.max.apply(Math, values.map(function(o) {return o;}))
     return Math.max.apply(null, values);
 }
 
