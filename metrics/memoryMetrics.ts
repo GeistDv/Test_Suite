d1e8310a249ef8889dbd1b6b94ca15e0da849e9d
automatafh <felipe.henao@testiando.co>
2022-11-22 15:18:02 -0500

    update prometheus memory metrics querystring

--- metrics/memoryMetrics.ts
+++ metrics/memoryMetrics.ts
@@ -1,6 +1,6 @@
 import Metrics from './Metrics';
 
-let queryString = 'container_memory_usage_bytes{namespace="santi"}';
+let queryString = 'container_memory_usage_bytes{namespace="santi", image="europe-west3-docker.pkg.dev/pwk-c4t-dev/internal-camino-dev/camino-node:tiedemann-64de0a0003bfab988da62850eef37ef01f82fdad-1668765791"}';
 let metrics = new Metrics();
 metrics.execPrometheus(queryString);
 
