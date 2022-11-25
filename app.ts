c47b5ffe0c6665074ff6bc54a96883b409f14905
Santiago Marin Rojas <nafhta@gmail.com>
2022-11-25 08:22:59 -0500

    data output

--- app.ts
+++ app.ts
@@ -123,7 +123,7 @@ app.post("/start", async (req, res) => {
 
         var metricProvider = new PrometeusProvider();
         await initPrivateKeys(dataFlow, testCase);
-        startTestsAndGatherMetrics(testCase, configType, i, metricProvider);
+        await startTestsAndGatherMetrics(testCase, configType, i, metricProvider);
     }
 
     console.log("Finished all tests");
